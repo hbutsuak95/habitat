dd6354f7a0217d490447c9ac84da9bfa930afc00
MikeS96 <miguel.saaruiz@gmail.com>
2022-09-21 11:59:57 -0400

    Corrected error with drifted transition and ground truth position during data collection

--- scripts/shortest_path_follower_scene.py
+++ scripts/shortest_path_follower_scene.py
@@ -4,11 +4,11 @@ import random
 import sys
 import json
 
-import cv2 
+import cv2
 import imageio
 import numpy as np
 import argparse
-import tqdm 
+import tqdm
 
 # function to display the topdown map
 from PIL import Image
@@ -20,20 +20,23 @@ from habitat_sim.utils import viz_utils as vut
 import habitat
 from habitat.core.utils import try_cv2_import
 from habitat.tasks.nav.shortest_path_follower import ShortestPathFollower
-from habitat_sim.nav import GreedyGeodesicFollower 
+from habitat_sim.nav import GreedyGeodesicFollower
 from habitat.utils.visualizations import maps
 from habitat.utils.visualizations.utils import images_to_video
+from habitat_sim.utils.common import quat_from_angle_axis
 
 from scipy.spatial.transform import Rotation as R
+
+from utils import split_data
 
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
-    	"--scene_id",
-    	type=str,
-    	default="./data/scene_datasets/habitat-test-scenes/skokloster-castle.glb",
-    	help="specify glb file ")
+        "--scene_id",
+        type=str,
+        default="./data/scene_datasets/habitat-test-scenes/skokloster-castle.glb",
+        help="specify glb file ")
     parser.add_argument(
         "--out_dir",
         default=os.path.join("data", "shortest_path_scene"),
@@ -46,10 +49,10 @@ def get_args():
         help="Number of episodes to collect data for",
     )
     parser.add_argument(
-    	"--max_steps",
-    	type=int,
-    	default=128,
-    	help="maximum steps allowed per episode")
+        "--max_steps",
+        type=int,
+        default=128,
+        help="maximum steps allowed per episode")
     parser.add_argument(
         "--min_steps",
         type=int,
@@ -75,13 +78,16 @@ def get_args():
         type=int,
         default=9211,
         help="set seed for random events and reproducibility")
+    parser.add_argument(
+        "--split",
+        type=float,
+        default=0.3,
+        help="Percentage of trajectories in validation set")
     args = parser.parse_args()
     return args
 
 
-
 def simulator_settings(rgb_sensor=True, depth_sensor=False):
-
     sim_settings = {
         "width": 256,  # Spatial resolution of the observations
         "height": 256,
@@ -96,6 +102,7 @@ def simulator_settings(rgb_sensor=True, depth_sensor=False):
         "angular_speed": args.angular_speed
     }
     return sim_settings
+
 
 def make_cfg(settings):
     '''
@@ -142,21 +149,20 @@ def make_cfg(settings):
             "turn_right", habitat_sim.agent.ActuationSpec(amount=settings["angular_speed"])
         ),
     }
-    
+
     cfg = habitat_sim.Configuration(sim_cfg, [agent_cfg])
 
     return cfg
-
 
 
 def euler_to_quaternion(yaw, pitch, roll):
     '''
         Converts Euler angles representation to Quaternion 
     '''
-    qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
-    qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
-    qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
-    qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+    qx = np.sin(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) - np.cos(roll / 2) * np.sin(pitch / 2) * np.sin(yaw / 2)
+    qy = np.cos(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2) + np.sin(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2)
+    qz = np.cos(roll / 2) * np.cos(pitch / 2) * np.sin(yaw / 2) - np.sin(roll / 2) * np.sin(pitch / 2) * np.cos(yaw / 2)
+    qw = np.cos(roll / 2) * np.cos(pitch / 2) * np.cos(yaw / 2) + np.sin(roll / 2) * np.sin(pitch / 2) * np.sin(yaw / 2)
 
     # return [qw, qx, qy, qz]
     return np.quaternion(qw, qx, qy, qz)
@@ -165,14 +171,18 @@ def euler_to_quaternion(yaw, pitch, roll):
 def get_step_dict(step, best_action, agent_state):
     step_dict = {}
     position, rot = agent_state.position, agent_state.rotation
-    step_dict["time"] = step; step_dict["actions"] = {'motion': best_action}
-    step_dict["reward"] = 0; step_dict["done"] = 0; step_dict["pose"] = {"x": float(position[0]), 'y': float(position[2]), 'z': float(position[1]),\
-     "orientation.x": rot.x, "orientation.y": rot.z, "orientation.z": rot.y, "orientation.w": rot.w}
-    return step_dict 
+    step_dict["time"] = step;
+    step_dict["actions"] = {'motion': best_action}
+    step_dict["reward"] = 0;
+    step_dict["done"] = 0;
+    step_dict["pose"] = {"x": float(position[0]), 'y': float(position[2]), 'z': float(position[1]), \
+                         "orientation.x": rot.x, "orientation.y": rot.z, "orientation.z": rot.y, "orientation.w": rot.w}
+    return step_dict
+
 
 def save_json(dict_, filename):
     with open(filename, "w") as f:
-        json.dump(dict_, f, indent=4) 
+        json.dump(dict_, f, indent=4)
 
 
 def collect_data(args, out_dir, seed=42):
@@ -197,54 +207,59 @@ def collect_data(args, out_dir, seed=42):
         obs = sim.reset()
         start_state = habitat_sim.AgentState()
         goal_state = habitat_sim.AgentState()
-        
+
         # Sample Random navigable points as start and goal states 
         start_state.position = sim.pathfinder.get_random_navigable_point()
         goal_state.position = sim.pathfinder.get_random_navigable_point()
-        
+
         # Randomly sample the rotations for start and goal states
-        start_state.rotation = euler_to_quaternion(0, random.randint(0, 360) * np.pi / 180, 0)
+        rot = quat_from_angle_axis(np.deg2rad(random.randint(-180, 180)), habitat_sim.geo.UP)
+        start_state.rotation = rot
+        # start_state.rotation = euler_to_quaternion(0, random.randint(0, 360) * np.pi / 180, 0)
         # goal_state.rotation = R.random().as_quat() # Not being used right now. 
-        
+
         # Set the agent start state
         agent = sim.initialize_agent(sim_settings["default_agent"])
         agent.set_state(start_state)
-        
+        sim.get_agent(0).state = start_state
+
         # Creating directories 
         try:
-            os.system("mkdir -p %s"%os.path.join(out_dir, "traj_%d"%episode, "images"))
-            os.system("mkdir -p %s"%os.path.join(out_dir, "traj_%d"%episode, "meta"))
+            os.system("mkdir -p %s" % os.path.join(out_dir, "traj_%d" % episode, "images"))
+            os.system("mkdir -p %s" % os.path.join(out_dir, "traj_%d" % episode, "meta"))
         except:
             pass
-        
+
         step = 0
         while step < args.max_steps:
 
             agent_state = agent.get_state()
             best_action = follower.next_action_along(goal_state.position)
             step_dict = get_step_dict(step, action_space.index(best_action), agent_state)
-            save_json(step_dict, os.path.join(out_dir, "traj_%d"%episode, "meta", "%d.json"%step))
+            save_json(step_dict, os.path.join(out_dir, "traj_%d" % episode, "meta", "%d.json" % step))
             if best_action is None:
                 break
-            obs = sim.step(best_action) 
-            img_path = os.path.join(out_dir, "traj_%d"%episode, "images", "%d.png"%step)
-            os.path.join(out_dir, "traj_%d"%episode, "meta", "%d.json"%step)
-            im = obs["color_sensor"][:,:,:3]
-            im = cv2.cvtColor(im,cv2.COLOR_RGB2BGR) # converting to BGR (as expected by the cv2.imwrite)
+            obs = sim.get_sensor_observations()
+            img_path = os.path.join(out_dir, "traj_%d" % episode, "images", "%d.png" % step)
+            os.path.join(out_dir, "traj_%d" % episode, "meta", "%d.json" % step)
+            im = obs["color_sensor"][:, :, :3]
+            im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)  # converting to BGR (as expected by the cv2.imwrite)
             cv2.imwrite(img_path, im)
+            # Do transition here!
+            obs = sim.step(best_action)
 
-            step+=1
+            step += 1
         # Save the last point in the trajectory as last step data and goal image
-        im = obs["color_sensor"][:,:,:3]
-        im = cv2.cvtColor(im,cv2.COLOR_RGB2BGR)
+        im = obs["color_sensor"][:, :, :3]
+        im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
         agent_state = agent.get_state()
-        cv2.imwrite(os.path.join(out_dir, "traj_%d"%episode, "goal.png"), im)            
+        cv2.imwrite(os.path.join(out_dir, "traj_%d" % episode, "goal.png"), im)
         # Save the goal state 
         goal_dict = get_step_dict(0, action_space.index(best_action), agent_state)
-        save_json(goal_dict, os.path.join(out_dir, "traj_%d"%episode, "goal.json"))
+        save_json(goal_dict, os.path.join(out_dir, "traj_%d" % episode, "goal.json"))
         # Rejects trajectories of length smaller than a given threshold 
-        if step < args.step_thresh: 
-            os.system("rm -rf %s"%os.path.join(out_dir, "traj_%d"%episode))
+        if step < args.step_thresh:
+            os.system("rm -rf %s" % os.path.join(out_dir, "traj_%d" % episode))
 
 
 def S1_fixed(args, out_dir, seed=42):
@@ -273,59 +288,66 @@ def S1_fixed(args, out_dir, seed=42):
         # Reset the environment 
         obs = sim.reset()
         goal_state = habitat_sim.AgentState()
-        
+
         # Randomly sample the rotations for start and goal states
-        start_state.rotation = euler_to_quaternion(0, random.randint(0, 360) * np.pi / 180, 0)
+        rot = quat_from_angle_axis(np.deg2rad(random.randint(-180, 180)), habitat_sim.geo.UP)
+        start_state.rotation = rot
+
+        # start_state.rotation = euler_to_quaternion(0, random.randint(0, 360) * np.pi / 180, 0)
         # goal_state.rotation = R.random().as_quat() # Not being used right now. 
-        
+
         # Set the agent start state
         agent = sim.initialize_agent(sim_settings["default_agent"])
         agent.set_state(start_state)
-        
+        sim.get_agent(0).state = start_state
+
         # Creating directories 
         try:
-            os.system("mkdir -p %s"%os.path.join(out_dir, "traj_%d"%episode, "images"))
-            os.system("mkdir -p %s"%os.path.join(out_dir, "traj_%d"%episode, "meta"))
+            os.system("mkdir -p %s" % os.path.join(out_dir, "traj_%d" % episode, "images"))
+            os.system("mkdir -p %s" % os.path.join(out_dir, "traj_%d" % episode, "meta"))
         except:
             pass
-        
 
-        # Randomly select number of steps for the episode 
-        num_steps = random.randint(args.min_steps, min(int(360/args.angular_speed), args.max_steps))
+        # Randomly select number of steps for the episode
+        num_steps = random.randint(args.min_steps, min(int(360 / args.angular_speed), args.max_steps))
 
         # Randomly select an action b/w left and right and keep executing 
         best_action = random.choice(["turn_left", "turn_right"])
 
         step = 0
         while step < num_steps:
-
             agent_state = agent.get_state()
             step_dict = get_step_dict(step, action_space.index(best_action), agent_state)
-            save_json(step_dict, os.path.join(out_dir, "traj_%d"%episode, "meta", "%d.json"%step))
-            obs = sim.step(best_action) 
-            img_path = os.path.join(out_dir, "traj_%d"%episode, "images", "%d.png"%step)
-            os.path.join(out_dir, "traj_%d"%episode, "meta", "%d.json"%step)
-            im = obs["color_sensor"][:,:,:3]
-            im = cv2.cvtColor(im,cv2.COLOR_RGB2BGR) # converting to BGR (as expected by the cv2.imwrite)
-            cv2.imwrite(img_path, im)
+            obs = sim.get_sensor_observations()
 
-            step+=1
+            save_json(step_dict, os.path.join(out_dir, "traj_%d" % episode, "meta", "%d.json" % step))
+            img_path = os.path.join(out_dir, "traj_%d" % episode, "images", "%d.png" % step)
+            os.path.join(out_dir, "traj_%d" % episode, "meta", "%d.json" % step)
+            im = obs["color_sensor"][:, :, :3]
+            im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)  # converting to BGR (as expected by the cv2.imwrite)
+            cv2.imwrite(img_path, im)
+            # Do the transition here! (Next image)
+            obs = sim.step(best_action)
+
+            step += 1
         # Save the last point in the trajectory as last step data and goal image
-        im = obs["color_sensor"][:,:,:3]
-        im = cv2.cvtColor(im,cv2.COLOR_RGB2BGR)
+        im = obs["color_sensor"][:, :, :3]
+        im = cv2.cvtColor(im, cv2.COLOR_RGB2BGR)
         agent_state = agent.get_state()
-        cv2.imwrite(os.path.join(out_dir, "traj_%d"%episode, "goal.png"), im)            
+        cv2.imwrite(os.path.join(out_dir, "traj_%d" % episode, "goal.png"), im)
         # Save the goal state 
         goal_dict = get_step_dict(0, action_space.index(best_action), agent_state)
-        save_json(goal_dict, os.path.join(out_dir, "traj_%d"%episode, "goal.json"))
+        save_json(goal_dict, os.path.join(out_dir, "traj_%d" % episode, "goal.json"))
         # Rejects trajectories of length smaller than a given threshold 
-        if step < args.step_thresh: 
-            os.system("rm -rf %s"%os.path.join(out_dir, "traj_%d"%episode))
+        if step < args.step_thresh:
+            os.system("rm -rf %s" % os.path.join(out_dir, "traj_%d" % episode))
 
 
-if __name__=="__main__":
+if __name__ == "__main__":
     args = get_args()
     random.seed(args.seed)
     np.random.seed(args.seed)
     out_dir = os.path.join(args.out_dir, args.scene_id.split("/")[-1][:-4])
-    S1_fixed(args, out_dir, seed=args.seed) # collect training data 
+    S1_fixed(args, out_dir, seed=args.seed)  # Collect training data
+    split_data(split=args.split, path=out_dir)
+    
